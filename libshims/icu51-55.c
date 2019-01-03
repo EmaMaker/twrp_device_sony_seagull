a89f41ddf8a7db2a269db4d5d0176a108ea55f47
EmaMaker <emamaker0@gmail.com>
2019-01-03 17:06:56 +0100

    First commit

---
+++ libshims/icu51-55.c
@@ -0,0 +1,170 @@
+#include <stdbool.h>
+#include "unicode/ucnv.h"
+
+/**
+THIS PART HAS BEEN TAKEN FROM:
+https://github.com/Nihhaar/android_device_xiaomi_mocha/blob/cm-13.0/libmocha/mocha_audio.c
+**/
+
+
+
+U_STABLE UConverter* U_EXPORT2
+ucnv_open_51(const char *converterName, UErrorCode *err)
+{
+    return ucnv_open(converterName, err);
+}
+
+U_STABLE void  U_EXPORT2
+ucnv_close_51(UConverter * converter)
+{
+    return ucnv_close(converter);
+}
+
+U_STABLE void U_EXPORT2 UCNV_TO_U_CALLBACK_STOP_51(
+                  const void *context,
+                  UConverterToUnicodeArgs *toUArgs,
+                  const char* codeUnits,
+                  int32_t length,
+                  UConverterCallbackReason reason,
+                  UErrorCode * err)
+{
+    UCNV_TO_U_CALLBACK_STOP(context, toUArgs, codeUnits, length, reason, err);
+}
+
+U_STABLE void U_EXPORT2 UCNV_FROM_U_CALLBACK_STOP_51(
+                  const void *context,
+                  UConverterFromUnicodeArgs *fromUArgs,
+                  const UChar* codeUnits,
+                  int32_t length,
+                  UChar32 codePoint,
+                  UConverterCallbackReason reason,
+                  UErrorCode * err)
+{
+    UCNV_FROM_U_CALLBACK_STOP (context, fromUArgs, codeUnits, length, codePoint, reason, err);
+}
+
+U_STABLE void U_EXPORT2
+ucnv_setToUCallBack_51(UConverter * converter,
+                     UConverterToUCallback newAction,
+                     const void* newContext,
+                     UConverterToUCallback *oldAction,
+                     const void** oldContext,
+                     UErrorCode * err)
+{
+    ucnv_setToUCallBack(converter, newAction, newContext, oldAction, oldContext, err);
+}
+
+U_STABLE void U_EXPORT2
+ucnv_setFromUCallBack_51(UConverter * converter,
+                       UConverterFromUCallback newAction,
+                       const void *newContext,
+                       UConverterFromUCallback *oldAction,
+                       const void **oldContext,
+                       UErrorCode * err)
+{
+    ucnv_setFromUCallBack(converter, newAction, newContext, oldAction, oldContext, err);
+}
+
+U_STABLE void U_EXPORT2
+ucnv_convertEx_51(UConverter *targetCnv, UConverter *sourceCnv,
+               char **target, const char *targetLimit,
+               const char **source, const char *sourceLimit,
+               UChar *pivotStart, UChar **pivotSource,
+               UChar **pivotTarget, const UChar *pivotLimit,
+               UBool reset, UBool flush,
+               UErrorCode *pErrorCode)
+{
+    ucnv_convertEx(targetCnv, sourceCnv, target, targetLimit, source, sourceLimit,
+                   pivotStart, pivotSource, pivotTarget, pivotLimit, reset, flush,
+                   pErrorCode);
+}
+
+void _ZN7android8MonoPipeC1Ejjb(size_t reqFrames, unsigned format_old, bool writeCanBlock)
+{
+
+}
+
+unsigned _ZN7android16Format_from_SR_CEjj(unsigned sampleRate, unsigned channelCount)
+{
+    return 0;
+}
+
+
+/**
+THIS PART HAS BEEN TAKEN FROM:
+https://github.com/vampirefo/android_device_blu_p6601/blob/master/libmtk_symbols/icu55.c
+**/
+
+
+U_STABLE UConverter* U_EXPORT2
+ucnv_open_55(const char *converterName, UErrorCode *err)
+{
+    return ucnv_open(converterName, err);
+}
+
+U_STABLE void  U_EXPORT2
+ucnv_close_55(UConverter * converter)
+{
+    return ucnv_close(converter);
+}
+
+U_STABLE void U_EXPORT2 UCNV_TO_U_CALLBACK_STOP_55(
+                  const void *context,
+                  UConverterToUnicodeArgs *toUArgs,
+                  const char* codeUnits,
+                  int32_t length,
+                  UConverterCallbackReason reason,
+                  UErrorCode * err)
+{
+    UCNV_TO_U_CALLBACK_STOP(context, toUArgs, codeUnits, length, reason, err);
+}
+
+U_STABLE void U_EXPORT2 UCNV_FROM_U_CALLBACK_STOP_55(
+                  const void *context,
+                  UConverterFromUnicodeArgs *fromUArgs,
+                  const UChar* codeUnits,
+                  int32_t length,
+                  UChar32 codePoint,
+                  UConverterCallbackReason reason,
+                  UErrorCode * err)
+{
+    UCNV_FROM_U_CALLBACK_STOP (context, fromUArgs, codeUnits, length, codePoint, reason, err);
+}
+
+U_STABLE void U_EXPORT2
+ucnv_setToUCallBack_55(UConverter * converter,
+                     UConverterToUCallback newAction,
+                     const void* newContext,
+                     UConverterToUCallback *oldAction,
+                     const void** oldContext,
+                     UErrorCode * err)
+{
+    ucnv_setToUCallBack(converter, newAction, newContext, oldAction, oldContext, err);
+}
+
+U_STABLE void U_EXPORT2
+ucnv_setFromUCallBack_55(UConverter * converter,
+                       UConverterFromUCallback newAction,
+                       const void *newContext,
+                       UConverterFromUCallback *oldAction,
+                       const void **oldContext,
+                       UErrorCode * err)
+{
+    ucnv_setFromUCallBack(converter, newAction, newContext, oldAction, oldContext, err);
+}
+
+U_STABLE void U_EXPORT2
+ucnv_convertEx_55(UConverter *targetCnv, UConverter *sourceCnv,
+               char **target, const char *targetLimit,
+               const char **source, const char *sourceLimit,
+               UChar *pivotStart, UChar **pivotSource,
+               UChar **pivotTarget, const UChar *pivotLimit,
+               UBool reset, UBool flush,
+               UErrorCode *pErrorCode)
+{
+    ucnv_convertEx(targetCnv, sourceCnv, target, targetLimit, source, sourceLimit,
+                   pivotStart, pivotSource, pivotTarget, pivotLimit, reset, flush,
+                   pErrorCode);
+}
+
+
